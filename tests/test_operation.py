9749d773c1b3c18b0eb4e1247b63a324805b459c
Lutfia23 <lutfianijhum.ruet.ece17@gmail.com>
2024-12-31 10:30:57 +0600

    Unit test case Updated

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,13 +1,17 @@
-from src.math_operation import add,sub
+# import sys
+# import os
 
+# Add the src directory to the Python path
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "D:/Github_Actions/src")))
+
+from src.math_operation import add, sub
 
 def test_add():
-    assert add(2,3)==5
-    assert add(-1,1)==0
-
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
 
 def test_sub():
-    assert sub(5,3)==2
-    assert sub(4,3)==1
-    assert sub(3,3)==0
-    assert sub(2,3)==-1
+    assert sub(5, 3) == 2
+    assert sub(4, 3) == 1
+    assert sub(3, 3) == 0
+    assert sub(2, 3) == -1
